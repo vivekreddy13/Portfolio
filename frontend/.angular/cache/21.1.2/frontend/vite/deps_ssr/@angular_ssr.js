4a80b30461a0e53946857de52c9af554fe785255
Vivek Reddy <vivekreddy@Viveks-MacBook-Pro-2.local>
2026-02-09 12:58:03 +0530

    merge from master to main

---
+++ frontend/.angular/cache/21.1.2/frontend/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,37 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-QOCZLZUW.js";
+import "./chunk-KY7IY2L5.js";
+import "./chunk-STR6BEU5.js";
+import "./chunk-OEN2L32I.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
